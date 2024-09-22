c0bc1dd6947daa8cc1b9d285890562eceb12b160
denism <den83178@gmail.com>
2024-09-22 19:32:29 +0300

    036 Btn Edit Delete. 037. Create method Edit to Controller, create View Edit.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -37,5 +37,21 @@ namespace Rocky.Controllers
             }
             return View(category);
         }
+
+        //Get - Edit
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            { 
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
