6e10db06d9e98988d92ad87d8c375058aede9616
denism <den83178@gmail.com>
2024-10-08 19:58:49 +0300

    045.046.047 Create method Upsert.Create inerface page GET-Upsert.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext prod)
+        {
+            _db = prod;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> ob_prod = _db.Product;
+
+            foreach (var ob_cat in ob_prod)
+            {
+                ob_cat.Category = _db.Category.FirstOrDefault(u => u.Id==ob_cat.CategoryId);
+            }
+            return View(ob_prod);
+        }
+
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+            if (id == null)
+            {
+                // this is for create
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+        }
+    }
+}
