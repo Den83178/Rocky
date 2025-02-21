23540418826be1c3d588e5d711504d22203bc536
DenisM <den83178@gmail.com>
2025-02-21 15:47:39 +0300

    072.073 Details method Post, Button remove from Cart.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
 using System.Diagnostics;
+using Rocky.Utility;
 
 namespace Rocky.Controllers
 {
@@ -31,13 +32,45 @@ namespace Rocky.Controllers
 
         public IActionResult Details(int id)
         {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)                                // IEnumerable<ShoppingCart> = List<ShoppingCart>
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);           // IEnumerable<ShoppingCart> !!!=== List<ShoppingCart>
+            }
+ 
+
             DetailsVM detailsVM = new DetailsVM();
             {
                 detailsVM.Product = _db.Product.Include(x => x.Category).Include(x => x.ApplicationType).Where(x => x.Id == id).FirstOrDefault();//??
                 detailsVM.ExistsInCart = false;
             };
+
+            foreach(var item in shoppingCartList)
+            {
+                if (item.ProductId == id)
+                {
+                    detailsVM.ExistsInCart = true;
+                }
+            }
             return View(detailsVM);
         }
+
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)                                // IEnumerable<ShoppingCart> = List<ShoppingCart>
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);           // IEnumerable<ShoppingCart> !!!=== List<ShoppingCart>
+            }
+
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
         public IActionResult Privacy()
         {
