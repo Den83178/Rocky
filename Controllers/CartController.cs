96507a167ce027fbd6a883cb2bbdddc858e20340
DenisM <den83178@gmail.com>
2025-03-02 11:37:20 +0300

    083. Creating CartControllers method Index. Getting model prodList has all product to Cart.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppinigCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count > 0)                                                     // may IEnumerable
+            {
+                //session exists
+                shoppinigCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppinigCartList.Select(i => i.ProductId).ToList();
+
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));     // alt first or default
+
+            return View(prodList);
+        }
+    }
+}
