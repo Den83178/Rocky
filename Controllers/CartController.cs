5e90e9111801d1bd435e9edcadc243b0fec3c249
DenisM <den83178@gmail.com>
2025-03-11 19:00:50 +0300

    087. The Method Get for Summary "button Continue"

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -3,13 +3,18 @@ using Rocky.Data;
 using Rocky.Models;
 using Rocky.Utility;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
-    [Authorize]
+    [Authorize]                                     //Microsoft.ASPNetCore.Authorization
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -31,6 +36,41 @@ namespace Rocky.Controllers
             return View(prodList);
         }
 
+        [HttpPost, ActionName("Index")]
+        [ValidateAntiForgeryToken]
+        
+        public IActionResult IndexPost()
+        {
+            
+
+            return RedirectToAction(nameof(Summary));
+        }
+
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppinigCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count > 0)                                                     // may IEnumerable
+            {
+                //session exists
+                shoppinigCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppinigCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));     // alt first or default
+
+            ProductUserVM = new ProductUserVM() 
+            {
+               ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+            };
+
+            return View(nameof(ProductUserVM));
+        }
+
 
         public IActionResult Remove(int id)
         {
@@ -49,6 +89,8 @@ namespace Rocky.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+
 
     }
 }
